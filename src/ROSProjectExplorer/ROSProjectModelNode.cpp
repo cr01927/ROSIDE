a54629c3c69cb9b27d0bde164e574baa5a386f36
Chris Reid <cr01927@georgiasouthern.edu>
2018-05-03 23:13:25 -0400

    Working ROSProjectExplorer to open files

---
+++ src/ROSProjectExplorer/ROSProjectModelNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cjreid on 5/3/18.
+//
+
+#include "ROSProjectModelNode.h"
---
+++ src/ROSProjectExplorer/ROSProjectModelNode.h
@@ -0,0 +1,14 @@
+//
+// Created by cjreid on 5/3/18.
+//
+
+#ifndef ROSIDE_ROSPROJECTMODELNODE_H
+#define ROSIDE_ROSPROJECTMODELNODE_H
+
+
+class ROSProjectModelNode {
+
+};
+
+
+#endif //ROSIDE_ROSPROJECTMODELNODE_H
